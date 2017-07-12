bfa42fa4687dd282ba25800727ab2cdf2a2f58f8
barcelosrui <barcelos.rui@gmail.com>
2017-07-12 23:57:50 +0100

    Insert to data

--- gestrepairdb.sql
+++ gestrepairdb.sql
@@ -32,6 +32,16 @@ CREATE TABLE `tbl_combustivel` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `tbl_combustivel`
+--
+
+LOCK TABLES `tbl_combustivel` WRITE;
+/*!40000 ALTER TABLE `tbl_combustivel` DISABLE KEYS */;
+INSERT INTO `tbl_combustivel` VALUES (1,'Gasolina Sem-Chumbo 95'),(2,'Gasoleo'),(3,'Gasolina Sem Chumbo 98'),(4,'GPL');
+/*!40000 ALTER TABLE `tbl_combustivel` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tbl_encomenda`
 --
 
@@ -50,6 +60,15 @@ CREATE TABLE `tbl_encomenda` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `tbl_encomenda`
+--
+
+LOCK TABLES `tbl_encomenda` WRITE;
+/*!40000 ALTER TABLE `tbl_encomenda` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_encomenda` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tbl_estado_orcamento`
 --
 
@@ -62,6 +81,15 @@ CREATE TABLE `tbl_estado_orcamento` (
   PRIMARY KEY (`id_estadoOrc`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_estado_orcamento`
+--
+
+LOCK TABLES `tbl_estado_orcamento` WRITE;
+/*!40000 ALTER TABLE `tbl_estado_orcamento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_estado_orcamento` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tbl_estado_reparacao`
@@ -78,6 +106,15 @@ CREATE TABLE `tbl_estado_reparacao` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `tbl_estado_reparacao`
+--
+
+LOCK TABLES `tbl_estado_reparacao` WRITE;
+/*!40000 ALTER TABLE `tbl_estado_reparacao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_estado_reparacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tbl_fornecedor`
 --
 
@@ -90,6 +127,15 @@ CREATE TABLE `tbl_fornecedor` (
   PRIMARY KEY (`id_fornecedor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_fornecedor`
+--
+
+LOCK TABLES `tbl_fornecedor` WRITE;
+/*!40000 ALTER TABLE `tbl_fornecedor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_fornecedor` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tbl_funcionario`
@@ -108,6 +154,15 @@ CREATE TABLE `tbl_funcionario` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `tbl_funcionario`
+--
+
+LOCK TABLES `tbl_funcionario` WRITE;
+/*!40000 ALTER TABLE `tbl_funcionario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_funcionario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tbl_marcaveiculo`
 --
 
@@ -121,6 +176,16 @@ CREATE TABLE `tbl_marcaveiculo` (
   UNIQUE KEY `marca` (`marca`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_marcaveiculo`
+--
+
+LOCK TABLES `tbl_marcaveiculo` WRITE;
+/*!40000 ALTER TABLE `tbl_marcaveiculo` DISABLE KEYS */;
+INSERT INTO `tbl_marcaveiculo` VALUES (4,'Ford'),(1,'Hyundai'),(3,'Mercedes Benz'),(2,'Volvo');
+/*!40000 ALTER TABLE `tbl_marcaveiculo` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tbl_modeloveiculo`
@@ -137,6 +202,16 @@ CREATE TABLE `tbl_modeloveiculo` (
   KEY `marcafk` (`marca`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_modeloveiculo`
+--
+
+LOCK TABLES `tbl_modeloveiculo` WRITE;
+/*!40000 ALTER TABLE `tbl_modeloveiculo` DISABLE KEYS */;
+INSERT INTO `tbl_modeloveiculo` VALUES (1,'H1',1),(2,'S80',2),(3,'B180',3),(4,'Fiesta',4);
+/*!40000 ALTER TABLE `tbl_modeloveiculo` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tbl_orcamentos`
@@ -161,6 +236,15 @@ CREATE TABLE `tbl_orcamentos` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `tbl_orcamentos`
+--
+
+LOCK TABLES `tbl_orcamentos` WRITE;
+/*!40000 ALTER TABLE `tbl_orcamentos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_orcamentos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tbl_peca`
 --
 
@@ -178,6 +262,15 @@ CREATE TABLE `tbl_peca` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `tbl_peca`
+--
+
+LOCK TABLES `tbl_peca` WRITE;
+/*!40000 ALTER TABLE `tbl_peca` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_peca` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tbl_peca_reparacao`
 --
 
@@ -191,6 +284,15 @@ CREATE TABLE `tbl_peca_reparacao` (
   KEY `pecafk` (`idpeca`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_peca_reparacao`
+--
+
+LOCK TABLES `tbl_peca_reparacao` WRITE;
+/*!40000 ALTER TABLE `tbl_peca_reparacao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_peca_reparacao` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tbl_pecafornecedor`
@@ -207,6 +309,15 @@ CREATE TABLE `tbl_pecafornecedor` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `tbl_pecafornecedor`
+--
+
+LOCK TABLES `tbl_pecafornecedor` WRITE;
+/*!40000 ALTER TABLE `tbl_pecafornecedor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_pecafornecedor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tbl_pecafornecida`
 --
 
@@ -221,6 +332,15 @@ CREATE TABLE `tbl_pecafornecida` (
   KEY `pecafornecidafornecedorFK` (`fornecedor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_pecafornecida`
+--
+
+LOCK TABLES `tbl_pecafornecida` WRITE;
+/*!40000 ALTER TABLE `tbl_pecafornecida` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_pecafornecida` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tbl_reparacao`
@@ -245,6 +365,15 @@ CREATE TABLE `tbl_reparacao` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `tbl_reparacao`
+--
+
+LOCK TABLES `tbl_reparacao` WRITE;
+/*!40000 ALTER TABLE `tbl_reparacao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_reparacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tbl_reparacao_funcionario`
 --
 
@@ -260,6 +389,15 @@ CREATE TABLE `tbl_reparacao_funcionario` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `tbl_reparacao_funcionario`
+--
+
+LOCK TABLES `tbl_reparacao_funcionario` WRITE;
+/*!40000 ALTER TABLE `tbl_reparacao_funcionario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_reparacao_funcionario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tbl_roles_utilizador`
 --
 
@@ -272,6 +410,16 @@ CREATE TABLE `tbl_roles_utilizador` (
   PRIMARY KEY (`idRole`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_roles_utilizador`
+--
+
+LOCK TABLES `tbl_roles_utilizador` WRITE;
+/*!40000 ALTER TABLE `tbl_roles_utilizador` DISABLE KEYS */;
+INSERT INTO `tbl_roles_utilizador` VALUES (1,'Administrador'),(2,'Gestor'),(3,'Funcionário'),(4,'Cliente');
+/*!40000 ALTER TABLE `tbl_roles_utilizador` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tbl_service`
@@ -289,6 +437,16 @@ CREATE TABLE `tbl_service` (
   PRIMARY KEY (`idService`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_service`
+--
+
+LOCK TABLES `tbl_service` WRITE;
+/*!40000 ALTER TABLE `tbl_service` DISABLE KEYS */;
+INSERT INTO `tbl_service` VALUES (1,'Pintura',25,'Pintura de Automóveis','serviceDefphoto.jpeg'),(2,'Revisão',20,'Mudança do Oleo','serviceDefphoto.jpeg'),(3,'Bate-Chapas',20,'Reparação ou Substituição de Chapas','serviceDefphoto.jpeg');
+/*!40000 ALTER TABLE `tbl_service` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tbl_utilizadores`
@@ -318,6 +476,16 @@ CREATE TABLE `tbl_utilizadores` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `tbl_utilizadores`
+--
+
+LOCK TABLES `tbl_utilizadores` WRITE;
+/*!40000 ALTER TABLE `tbl_utilizadores` DISABLE KEYS */;
+INSERT INTO `tbl_utilizadores` VALUES (1,'Rui Barcelos','Rua João Maria da Costa n9B','2090-093','Alpiarça','aluno18834@ipt.pt','241804744','911909758','rbarcelos','46f94c8de14fb36680850768ff1b7f2a',NULL,1,1),(2,'Miguel Silva','Vale de Santarém','2005-712','Vale de Santarém','miguelflsilva1995@gmail.com','123123122','919191991','migsilva','46f94c8de14fb36680850768ff1b7f2a',NULL,1,2),(3,'João Bandarra','Agrantes','2022-024','Amrantes','a@f.pt','223123121','987487987','bandarra','46f94c8de14fb36680850768ff1b7f2a',NULL,1,3),(4,'Pedro Vicente Nunes','Matas','2025-022','Santarém','b@a.pt','123212323','912911911','pnunes','46f94c8de14fb36680850768ff1b7f2a',NULL,1,4),(5,'João Bandarra','Agrantes','2022-021','Amrantes','a@f.pt','123123121','987487987','jbandarra','46f94c8de14fb36680850768ff1b7f2a',NULL,1,4),(6,'Oscar Taquara Cardozo','Rua x','2090-099','Alpiarça','cardozo@ipt.pt','414999444','911922923','cardozo','46f94c8de14fb36680850768ff1b7f2a',NULL,1,4),(7,'João Canoso','Alpiarça','2090-022','Alpiarça','aluno18839@ipt.pt','233244255','911911991','jcanoso','46f94c8de14fb36680850768ff1b7f2a',NULL,1,4),(8,'Jonas Pistolas','Caixa Futebol Campus','9999-000','Seixal','jonas@slbenfica.pt','881132222','991122991','jonas','46f94c8de14fb36680850768ff1b7f2a',NULL,0,4);
+/*!40000 ALTER TABLE `tbl_utilizadores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tbl_utilizadores_veiculos`
 --
 
@@ -332,6 +500,16 @@ CREATE TABLE `tbl_utilizadores_veiculos` (
   KEY `veifk` (`numVei`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_utilizadores_veiculos`
+--
+
+LOCK TABLES `tbl_utilizadores_veiculos` WRITE;
+/*!40000 ALTER TABLE `tbl_utilizadores_veiculos` DISABLE KEYS */;
+INSERT INTO `tbl_utilizadores_veiculos` VALUES (1,1,1),(1,2,1),(2,3,1);
+/*!40000 ALTER TABLE `tbl_utilizadores_veiculos` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tbl_veiculo`
@@ -357,6 +535,70 @@ CREATE TABLE `tbl_veiculo` (
   KEY `combustivel` (`combustivel`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_veiculo`
+--
+
+LOCK TABLES `tbl_veiculo` WRITE;
+/*!40000 ALTER TABLE `tbl_veiculo` DISABLE KEYS */;
+INSERT INTO `tbl_veiculo` VALUES (1,'99-33-HH',1,22,33,1991999,1,'99','99',1997),(2,'88-00-XX',2,3,43,222333,2,'33','33',2004),(3,'88-22-ZZ',3,22,34,133223,1,'33','22',2005);
+/*!40000 ALTER TABLE `tbl_veiculo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'gestrepair'
+--
+
+--
+-- Dumping routines for database 'gestrepair'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `infouser` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `infouser`(IN `iduser` INT)
+BEGIN 
+select tbauth.username as username, tbauth.password as password, tbuser.nome as nome, tbuser.morada as morada, tbuser.codPostal as codPostal, tbuser.localidade as localidade, tbuser.email as email, tbuser.nif as nif, tbuser.contacto as contacto, tbroleuser.nomeRole 
+from tbl_autenticacao tbauth, tbl_utilizadores tbuser, tbl_roles_utilizador tbroleuser 
+WHERE tbauth.utilizador = tbuser.numUtilizador 
+and tbauth.role = tbroleuser.idRole
+and tbauth.utilizador = iduser 
+LIMIT 1;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `listuser` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `listuser`()
+BEGIN 
+select `tbuser`.`numUtilizador` as `numutilizador`, `tbauth`.`username` as `username`, `tbuser.nome` as `nome`, `tbuser`.`morada` as `morada`, `tbuser`.`codPostal` as `codPostal`, `tbuser`.`localidade` as `localidade`, `tbuser`.`email` as `email`, `tbuser`.`nif` as `nif`, `tbuser`.`contacto` as `contacto`, `tbroleuser`.`nomeRole` 
+from `tbl_autenticacao` `tbauth`, `tbl_utilizadores` `tbuser`, `tbl_roles_utilizador` `tbroleuser` 
+WHERE `tbauth`.`utilizador` = `tbuser`.`numUtilizador`
+and `tbauth`.`role` = `tbroleuser`.`idRole`;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -367,4 +609,4 @@ CREATE TABLE `tbl_veiculo` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-12 23:44:56
+-- Dump completed on 2017-07-12 23:57:06
