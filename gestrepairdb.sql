22dec766ce73abc3b32cf8ee9da234fa4cf9f0a2
barcelosrui <barcelos.rui@gmail.com>
2017-07-31 14:53:26 +0100

    New DAMP

--- gestrepairdb.sql
+++ gestrepairdb.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `gestrepair` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `gestrepair`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: gestrepair
@@ -564,4 +566,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-31 13:48:55
+-- Dump completed on 2017-07-31 14:53:02
